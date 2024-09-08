49d343eda2fc7cc2d4466eb2687fd20358856451
rers <rodisenrique73@gmail.com>
2024-09-08 16:00:21 -0500

    feat: add User details and principal

--- server/src/main/java/com/nocountry/server/model/entity/User.java
+++ server/src/main/java/com/nocountry/server/model/entity/User.java
@@ -1,13 +1,25 @@
 package com.nocountry.server.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Principal;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-@Data
-@Table(name = "usuario")
-public class User {
+@Table(name = "users")
+public class User implements UserDetails, Principal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +31,7 @@ public class User {
     @Column(name = "lastname")
     private String lastName;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
@@ -28,16 +41,56 @@ public class User {
     @Column(name = "url_image")
     private String urlImage;
 
-    private boolean enabled;
+    @Column(name = "is_enabled")
+    private boolean isEnabled;
 
-    @Column(name = "account_locked")
-    private boolean accountLocked;
+    @Column(name = "is_account_locked")
+    private boolean isAccountLocked;
 
-    @OneToOne
-    @JoinColumn(name = "professional_id")
-    private Professional professional;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public String fullName() {
         return firstName + " " + lastName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleEnum().name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !isAccountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return email;
     }
 }
