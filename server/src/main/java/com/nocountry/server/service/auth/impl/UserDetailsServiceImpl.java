f7a8e3d8c21a016eb95041a281893f8862e470d1
rers <rodisenrique73@gmail.com>
2024-09-08 16:19:47 -0500

    feat: add user details

---
+++ server/src/main/java/com/nocountry/server/service/auth/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nocountry.server.service.auth.impl;
+
+import com.nocountry.server.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
