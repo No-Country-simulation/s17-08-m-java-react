639d60a8b6d05b64c55133f50ffce60bca3f72de
rers <rodisenrique73@gmail.com>
2024-09-10 14:45:06 -0500

    feat: add security exceptions
    handle exception disabled account,locked account and bad credentials

--- server/src/main/java/com/nocountry/server/config/advice/GlobalExceptionHandler.java
+++ server/src/main/java/com/nocountry/server/config/advice/GlobalExceptionHandler.java
@@ -3,12 +3,36 @@ package com.nocountry.server.config.advice;
 import com.nocountry.server.utils.ExceptionResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSendException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MailSendException.class)
+    public ResponseEntity<Object> mailSendException(MailSendException exception, HttpServletRequest request) {
+        return ResponseEntity.status(500).body(ExceptionResponse.create(exception, request, "Failed to connect to the mail server."));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Object> badCredentialsException(BadCredentialsException exception, HttpServletRequest request) {
+        return ResponseEntity.status(401).body(ExceptionResponse.create(exception, request, "username or password is incorrect."));
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<Object> lockedException(LockedException exception, HttpServletRequest request) {
+        return ResponseEntity.status(401).body(ExceptionResponse.create(exception, request, "Your account is locked."));
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<Object> disabledException(DisabledException exception, HttpServletRequest request) {
+        return ResponseEntity.status(401).body(ExceptionResponse.create(exception, request, "Your account is disabled."));
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> exception(Exception exception, HttpServletRequest request) {
@@ -19,6 +43,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
         return ResponseEntity.status(400).body(ExceptionResponse.create(exception, request));
     }
-
-
 }
