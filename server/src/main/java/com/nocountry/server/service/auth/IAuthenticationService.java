49c6c9f0d5abd713fcdab3b378936f4e580c43e3
rers <rodisenrique73@gmail.com>
2024-09-09 22:09:55 -0500

    feat: add sign up method

--- server/src/main/java/com/nocountry/server/service/auth/IAuthenticationService.java
+++ server/src/main/java/com/nocountry/server/service/auth/IAuthenticationService.java
@@ -3,8 +3,11 @@ package com.nocountry.server.service.auth;
 
 import com.nocountry.server.model.dto.auth.AuthenticationRequest;
 import com.nocountry.server.model.dto.auth.AuthenticationResponse;
+import com.nocountry.server.model.dto.auth.UserRegistrationRequest;
 
 public interface IAuthenticationService {
 
     AuthenticationResponse authenticate(AuthenticationRequest request);
+
+    void signUp(UserRegistrationRequest request);
 }
