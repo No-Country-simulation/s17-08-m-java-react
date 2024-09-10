fbe72c308467ca904d69dc63d880fef47fd32e53
rers <rodisenrique73@gmail.com>
2024-09-09 22:33:16 -0500

    feat: add sign up endpoint

--- server/src/main/java/com/nocountry/server/controller/AuthenticationController.java
+++ server/src/main/java/com/nocountry/server/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.nocountry.server.controller;
 
 import com.nocountry.server.model.dto.auth.AuthenticationRequest;
 import com.nocountry.server.model.dto.auth.AuthenticationResponse;
+import com.nocountry.server.model.dto.auth.UserRegistrationRequest;
 import com.nocountry.server.service.auth.IAuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,14 @@ public class AuthenticationController {
 
     private final IAuthenticationService authService;
 
-    @PostMapping("/login")
+    @PostMapping("/log-in")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register() {
-        return null;
+    @PostMapping("/sign-up")
+    public ResponseEntity<Void> register(@RequestBody UserRegistrationRequest request) {
+        authService.signUp(request);
+        return ResponseEntity.status(201).build();
     }
 }
