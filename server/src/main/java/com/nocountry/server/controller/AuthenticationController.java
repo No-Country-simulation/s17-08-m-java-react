a8fb7d8cd612440635630819721eecf2cd58c2b7
rers <rodisenrique73@gmail.com>
2024-09-10 14:35:07 -0500

    feat: validate dtos

--- server/src/main/java/com/nocountry/server/controller/AuthenticationController.java
+++ server/src/main/java/com/nocountry/server/controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.nocountry.server.model.dto.auth.AuthenticationRequest;
 import com.nocountry.server.model.dto.auth.AuthenticationResponse;
 import com.nocountry.server.model.dto.auth.UserRegistrationRequest;
 import com.nocountry.server.service.auth.IAuthenticationService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +17,20 @@ public class AuthenticationController {
 
     private final IAuthenticationService authService;
 
+    @SecurityRequirements
     @PostMapping("/log-in")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
+    @SecurityRequirements
     @PostMapping("/sign-up")
-    public ResponseEntity<Void> register(@RequestBody UserRegistrationRequest request) {
+    public ResponseEntity<Void> register(@RequestBody @Valid UserRegistrationRequest request) {
         authService.signUp(request);
         return ResponseEntity.status(201).build();
     }
 
+    @SecurityRequirements
     @PostMapping("validate-account")
     public ResponseEntity<Void> validateAccount(@RequestParam String token) {
         authService.activateAccount(token);
