f9340019be76e2851d8c91fe1dd9c93d5a759a1c
rers <rodisenrique73@gmail.com>
2024-09-12 10:28:58 -0500

    feat: rename paths and add basic swagger doc

--- server/src/main/java/com/nocountry/server/controller/UserController.java
+++ server/src/main/java/com/nocountry/server/controller/UserController.java
@@ -4,6 +4,7 @@ import com.nocountry.server.exception.UserNotFoundException;
 import com.nocountry.server.model.dto.UserDto;
 import com.nocountry.server.model.entity.User;
 import com.nocountry.server.service.impl.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +14,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Users")
 @RestController
-@RequestMapping("/arregloYa/v1/")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
     private final UserService service;
 
-    @GetMapping("user/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         try {
             User user = service.findById(id);
@@ -33,14 +35,14 @@ public class UserController {
         }
     }
 
-    @GetMapping("users")
+    @GetMapping
     public ResponseEntity<?> listUsers(){
         List<User> list= service.getAllUser();
         return new ResponseEntity<>(list, HttpStatus.OK);
 
    }
 
-    @PostMapping("user")
+    @PostMapping
     public ResponseEntity<?> createUser(@RequestBody @Valid UserDto dto) {
         if (service.createUser(dto)) {
             log.info("User Created successfully with the email : {}", dto.getEmail());
@@ -52,7 +54,7 @@ public class UserController {
 
     }
 
-    @PutMapping("user/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@RequestBody @Valid UserDto dto, @PathVariable Long id){
         User user = service.updateUser(dto, id);
         if (user != null){
@@ -65,7 +67,7 @@ public class UserController {
 
     }
 
-    @DeleteMapping("user/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id){
         if(service.deleteUser(id)){
             log.info("User deleted with id: {}", id);
