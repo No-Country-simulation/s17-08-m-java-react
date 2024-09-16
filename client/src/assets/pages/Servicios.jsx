c6553dcb076b9d1304833a32adb30b817c60a135
Leonel Amaya <omar.mejia2099@gmail.com>
2024-09-16 16:21:56 -0500

    feat: new services page

---
+++ client/src/assets/pages/Servicios.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Services from "../components/Service";
+
+function Servicios() {
+    return (
+        <div className="container mx-auto py-16 space-y-10">
+            <h1 className="text-center text-gray-800 text-3xl font-bold mb-2">
+                ¿Qué servicios se ofrecen?
+            </h1>
+
+            <div className="flex flex-wrap items-center justify-center">
+                {/* Contenedor del texto */}
+                <div className="w-full lg:w-1/2 lg:pl-2 text-left">
+                    <p>
+                        En ArregloYa, puedes encontrar una amplia gama de profesionales en
+                        diferentes campos y especializadades.
+                    </p>
+                    <br />
+                    <p>
+                        Podés encontrar profesionales para soluciones de alabañileria,
+                        plomería, carpintería 
+                    </p>
+                    <br />
+                    <p className="font-bold text-center">¡Y muchos más!</p>
+                </div>
+
+
+            </div>
+            <Services />
+        </div>
+    );
+}
+
+export default Servicios;
